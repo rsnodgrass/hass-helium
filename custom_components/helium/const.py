525a91f0b1038c52d9d6824331ef94b46f598c34
Ryan Snodgrass <rsnodgrass@gmail.com>
2021-06-03 02:10:18 -0700

    update

--- custom_components/helium/const.py
+++ custom_components/helium/const.py
@@ -4,6 +4,8 @@ DOMAIN = 'helium'
 
 ATTRIBUTION = 'Data from Helium Blockchain'
 
+DEFAULT_TIMEOUT = 15
+
 ATTR_ADDRESS = 'address'
 ATTR_ATTRIBUTION = 'attribution'
 ATTR_BLOCK = 'block'
@@ -11,6 +13,7 @@ ATTR_TIMESTAMP = 'timestamp'
 
 CONF_WALLET = 'wallets'
 CONF_HOTSPOT = 'hotspots'
+CONF_TIMEOUT = 'timeout'
 
 #ICON_HOTSPOT = "mdi:access-point"
 ICON_HOTSPOT = 'mdi:router-wireless'
